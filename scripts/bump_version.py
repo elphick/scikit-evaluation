8f4955b150ead45d68525e0bc833b654abc84338
Greg <11791585+elphick@users.noreply.github.com>
2025-02-04 18:38:37 +0800

    Fixes #64 - added partitionestimator unit tests.  improved changelog.

--- scripts/bump_version.py
+++ scripts/bump_version.py
@@ -1,11 +1,17 @@
+
 import argparse
 import subprocess
 import sys
 
 
 def run_command(command):
-    process = subprocess.Popen(command, shell=True)
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     process.wait()
+
+
+def run_towncrier():
+    process = subprocess.Popen('towncrier', stdin=subprocess.PIPE, shell=True)
+    process.communicate(input=b'N\n')
 
 
 def process_command_line_parameters():
@@ -13,6 +19,23 @@ def process_command_line_parameters():
     parser.add_argument('increment', type=str, help='The increment type (major, minor, patch)')
     args = parser.parse_args()
     return args
+
+
+def adjust_changelog():
+    with open('CHANGELOG.rst', 'r') as file:
+        lines = file.readlines()
+
+    # Remove 'Elphick.' prefix from the first line
+    prefix = 'Elphick.'
+    if lines[0].startswith(prefix):
+        lines[0] = lines[0][len(prefix):]
+
+    # Adjust the length of the underline on the second line
+    if lines[1].startswith('='):
+        lines[1] = '=' * (len(lines[0].strip())) + '\n'  # -1 for the newline character
+
+    with open('CHANGELOG.rst', 'w') as file:
+        file.writelines(lines)
 
 
 def main():
@@ -26,9 +49,15 @@ def main():
 
     # Run the commands
     run_command(f"poetry version {increment}")
-    run_command("poetry install")
-    # run_command("python towncrier/create_news.py")
-    run_command("towncrier")
+    run_command("poetry install --all-extras")
+
+    run_towncrier()
+
+    # remove the news fragments manually.
+    run_command("rm -rf ./towncrier/newsfragments/*")
+
+    # strip the Elphick. prefix from the top heading only.
+    adjust_changelog()
 
 
 if __name__ == "__main__":
